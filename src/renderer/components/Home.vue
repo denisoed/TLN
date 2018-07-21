0644d588a784fdf4cd2624284454d2415579a40c
Denisoed <denisod93@gmail.com>
2018-07-22 00:02:46 +0600

    Added action item

--- src/renderer/components/Home.vue
+++ src/renderer/components/Home.vue
@@ -1,12 +1,33 @@
 <template>
-    <section>
-      <h1>Home</h1>
+    <section class="home">
+      <div class="acrion-item_list">
+        <actionItem v-if="actionItems.length > 0" v-for="(item, index) in actionItems" :key="`action-${index}`" :actionInfoData="item"/>
+      </div>
     </section>
 </template>
 
 <script>
+  import ActionItem from './blocks/actionItem'
+
   export default {
-    name: 'home'
+    name: 'home',
+    data: function () {
+      return {
+        actionItems: [
+          {
+            title: 'Dead line',
+            time: '1h 40m'
+          },
+          {
+            title: 'Go home',
+            time: '4h 10m'
+          },
+        ],
+      }
+    },
+    components: {
+      actionItem: ActionItem,
+    }
   }
 </script>
 
---
+++ src/renderer/components/blocks/actionItem.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="action-item">
+        <div class="action-item_info">
+            <h4>{{ this.actionInfoData.title }}</h4>
+            <p>{{ this.actionInfoData.time }}</p>
+        </div>
+        <button class="start-action"><icon name="stopwatch"/></button>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'action-item',
+    props: ['actionInfoData']
+  }
+</script>
+
+<style>
+
+</style>
