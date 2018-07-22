805501ff5c3587f4a256d45328ab305021e5a45f
Denisoed <denisod93@gmail.com>
2018-07-22 11:24:04 +0600

    added sound for natification finished action

--- src/renderer/components/blocks/actionItem.vue
+++ src/renderer/components/blocks/actionItem.vue
@@ -5,20 +5,25 @@
             <p>{{ this.actionInfoData.minutes }}</p>
             <div class="timer">{{ this.prettyTime | prettify }}</div>
         </div>
-        <button class="start-action" @click="runAction" v-if="!isRunning"><icon name="stopwatch"/></button>
-        <button class="start-action" @click="pauseAction" v-if="isRunning"><icon name="pause"/></button>
+        <button class="start-action" @click="runAction" v-if="!isRunning && !ifFinish"><icon name="stopwatch"/></button>
+        <button class="start-action" @click="pauseAction" v-if="isRunning && !ifFinish"><icon name="pause"/></button>
+        <button class="start-action" @click="finishAction" v-if="ifFinish"><icon name="times"/></button>
     </div>
 </template>
 
 <script>
+  import sound from '../../assets/sound.mp3'
+
   export default {
     name: 'action-item',
     props: ['actionInfoData'],
     data: function () {
       return {
         isRunning: false,
+        ifFinish: false,
         time: (this.actionInfoData.minutes * 60),
         timer:null,
+        sound:new Audio(sound)
       }
     },
     filters: {
@@ -52,6 +57,9 @@
                         this.time--
                     } else {
                         clearInterval(this.timer)
+                        this.ifFinish = true
+                        this.sound.loop = true
+                        this.sound.play()
                     }
                 }, 1000 )
             }
@@ -60,6 +68,11 @@
             this.isRunning = false
             clearInterval(this.timer)
             this.timer = null
+        },
+        finishAction() {
+            this.ifFinish = false
+            this.isRunning = false
+            this.sound.pause()
         },
     }
   }
