69a071923cbab35bd228bbdb403524422868dbbf
denisoed <denisod93@gmail.com>
2018-07-25 22:15:52 +0600

    Aside menu

--- src/renderer/components/Aside.vue
+++ src/renderer/components/Aside.vue
@@ -1,37 +1,67 @@
 <template>
-    <aside class="aside-bar">
-      <div class="create-new-action">
-        <button class="open-action-modal" @click="openActionModal"><icon name="plus"/></button>
-        <createAction @EventCloseActionModal="closeActionModal" v-if="actionModal"></createAction>
-      </div>
-      <router-link class="aside-item link-sprint" to="/sprint"><icon name="th-large"/></router-link>
-    </aside>
+  <div class="sidebar">
+    <div class="logo">
+    </div>
+    <div class="menu">
+        <div class="menu_icons">
+          <a href="#"><icon name="clone" size="lg"></icon></a>
+          <a href="#"><icon name="user-friends"></icon></a>
+          <a href="#"><icon name="user"></icon></a>
+          <a href="#"><icon name="file-alt"></icon></a>
+          <a href="#"><icon name="comments"></icon></a>
+          <a href="#"><icon name="cog"></icon></a>
+        </div>
+    </div>
+  </div>
 </template>
 
 <script>
   import CreateAction from './blocks/createAction'
   
   export default {
-    name: 'aside-bar',
+    name: 'side-bar',
     data: function () {
       return {
-        actionModal: false,
       }
     },
     components: {
-      createAction: CreateAction,
     },
     methods: {
-      openActionModal() {
-        this.actionModal = true;
       },
-      closeActionModal(event) {
-        this.actionModal = event;
-      }
-    },
-  }
+    }
 </script>
 
 <style>
-
+  .sidebar  {
+    width: 100px;
+    height: 1000px;
+    background-color: #fff;
+    border-right: 1px solid #D3D3D3
+  }
+  .logo {
+    height: 110px;
+  }
+  .menu {
+    height: 100%;
+    margin-top: 40px;
+  }
+  .menu_icons {
+    display: flex;
+    flex-direction: column;
+  }
+  .menu_icons a {
+    text-decoration: none;
+    color: #20B2AA;
+    width: 100%;
+    height: 60px;
+    border: 5px solid transparent;
+  }
+  .menu_icons svg {
+    height: 22px;
+    width: 22px;
+    margin: 20px 35px;
+  }
+  .menu_icons a:hover {
+    border-left: 5px #20B2AA solid;
+  }
 </style>
--- src/renderer/components/Sprint.vue
+++ src/renderer/components/Sprint.vue
@@ -1,17 +1,5 @@
 <template>
     <section class="sprint">
-      <div class="sprint-header">
-        <h2>Sprints</h2>
-        <div class="new-sprint">
-          <button class="create-new-sprint" @click="openSprintModal"><icon name="plus" /> New Sprint</button>
-          <createSprint @EventCloseSprintModal="closeSprintModal" v-if="sprintModal"/>
-        </div>
-      </div>
-      <div class="sprint-body">
-        <div class="sprint-list">
-          <sprintItem v-if="sprintItems.length > 0" v-for="(item, index) in sprintItems" :key="`sprint-${index}`" :sprintInfoData="item"/>
-        </div>
-      </div>
     </section>
 </template>
 
@@ -23,23 +11,13 @@
     name: 'sprint',
     data: function () {
       return {
-        sprintModal: false,
-        sprintItems: this.$store.getters.getSprintList
       }
     },
     components: {
-      createSprint: CreateSprint,
-      sprintItem: SprintItem,
     },
     methods: {
-      openSprintModal() {
-        this.sprintModal = true
       },
-      closeSprintModal(event) {
-        this.sprintModal = event;
-      }
-    },
-  }
+    }
 </script>
 
 <style>
