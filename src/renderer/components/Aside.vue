195af4eb85f9edb5089a58e29f43c8ca81822b74
denisoed <denisod93@gmail.com>
2018-07-29 16:54:53 +0600

    gave new style to aside menu

--- src/renderer/components/Aside.vue
+++ src/renderer/components/Aside.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="sidebar">
     <div class="logo">
+      <img src="../assets/time_logo.svg" alt="">
     </div>
     <div class="menu">
         <div class="menu_icons">
           <router-link to="/sprints"><icon name="th-large"/></router-link>
-          <a href="#"><icon name="user-friends"></icon></a>
+          <a href="#"><icon name="bolt"></icon></a>
           <a href="#"><icon name="user"></icon></a>
-          <a href="#"><icon name="file-alt"></icon></a>
-          <a href="#"><icon name="comments"></icon></a>
-          <a href="#"><icon name="cog"></icon></a>
+          <a href="#"><icon name="clipboard-list"></icon></a>
+          <a href="#"><icon name="calendar-alt"></icon></a>
+          <a href="#"><icon name="clock"></icon></a>
         </div>
     </div>
+    <button class="settings_button"><icon name="cog"></icon></button>
   </div>
 </template>
 
