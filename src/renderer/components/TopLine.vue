5794d0b22cedebc1a5be84f9f55c56dcd6b0248b
AntonGrushkin <toha_czar@mail.ru>
2018-08-06 22:39:47 +0600

    added top-line menu/ added styles for top line menu/ added fonts

---
+++ src/renderer/components/TopLine.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="top_line">
+      <div class="top-line-menu">
+        <div class="left-side">
+          <div class="logo">
+            <icon name="chart-pie"></icon>
+          </div>
+          </div>
+          <div class="right-side">
+              <a href="#" class="btn-go-pro">GO PRO</a>
+            <div class="btn-log-sign">
+              <a href="#">Log in</a>
+              <a href="#">Sign up</a>
+            </div>
+          </div>
+        </div>
+      </div>
+</template>
+
+<script>
+  
+  export default {
+    name: 'top_line',
+    data: function () {
+      return {
+      }
+    },
+    components: {
+    },
+    methods: {
+      },
+    }
+</script>
